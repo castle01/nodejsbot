78d917fdc154543a2282631e9b94f2b00a3bc3dc
hdorm518 <wnrehqhd1@naver.com>
2020-07-01 20:43:19 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,93 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+const welcomeChannelName = "welcome";
+const welcomeChannelComment = "어서오소리.";
+
+
+client.on('ready', () => {
+  console.log('ON!');
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+
+  member.addRole(guild.roles.find(role => role.name == "Guest"));
+});
+
+  client.on('message', (message) => {
+    if(message.author.bot) return;
+  
+    if(message.content == 'ping') {
+      return message.reply('poooooooooooooooooooooong');
+    }
+  
+    if(message.content == '!안녕') {
+      let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
+      let embed = new Discord.RichEmbed()
+        .setTitle('타이틀')
+        .setURL('http://www.naver.com')
+        .setAuthor('HUKA', img, 'http://www.naver.com')
+        .setThumbnail(img)
+        .addBlankField()
+        .addField('Inline field title', 'Some value here')
+        .addField('Inline field title', 'Some value here', true)
+        .addField('Inline field title', 'Some value here', true)
+        .addField('Inline field title', 'Some value here', true)
+        .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
+        .addBlankField()
+        .setTimestamp()
+        .setFooter('보고만듬', img)
+  
+      message.channel.send(embed)
+    } else if(message.content == 'embed2') {
+      let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
+      let commandList = [
+        {name: 'ping', desc: '현재 핑 상태'},
+        {name: 'embed', desc: 'embed 예제1'},
+        {name: 'embed2', desc: 'embed 예제2 (help)'},
+      ];
+      let commandStr = '';
+      let embed = new Discord.RichEmbed()
+        .setAuthor('Help of HUKA BOT', helpImg)
+        .setColor('#186de6')
+        .setFooter(`HUKA BOT ❤️`)
+        .setTimestamp()
+      
+      commandList.forEach(x => {
+        commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
+      });
+  
+      embed.addField('Commands: ', commandStr);
+  
+      message.channel.send(embed)
+    }
+  
+    
+  });
+  
+  function changeCommandStringLength(str, limitLen = 8) {
+    let tmp = str;
+    limitLen -= tmp.length;
+  
+    for(let i=0;i<limitLen;i++) {
+        tmp += ' ';
+    }
+  
+    return tmp;
+  }
+  
+  
+  client.login(token);
